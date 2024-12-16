9a82c76a3063cd654443d6a71707e3445ff1627d
9996rojit <rockman9996@gmail.com>
2024-12-16 21:57:37 +0545

    Managed issue in logging data

--- index.js
+++ index.js
@@ -2,14 +2,6 @@
 const net = require("net");
 const Gt06 = require('gt06')
 
-// TCP server configuration
-const tcpPort = 8090;
-
-// const byte = new Buffer.from('78781101086961106002852380411f4100007365f0d0a', 'hex')
-// const gt06 = new Gt06()
-// gt06.parse(byte)
-// console.log("++++++++++++++++++++++>", gt06)
-
 // Create a TCP server
 const server = net.createServer((socket) => {
   const gt06 = new Gt06();
@@ -27,6 +19,9 @@ const server = net.createServer((socket) => {
     }
 
     if (gt06.expectsResponse && gt06.event?.string === 'login') {
+      socket.write(gt06.responseMsg)
+    }
+    if (gt06.expectsResponse && gt06.event?.string === 'status') {
       socket.write(gt06.responseMsg)
     }
 
