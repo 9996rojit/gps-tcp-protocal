560798690f6d4bcdd858d39ceb6dc758f9a5bb14
9996rojit <rockman9996@gmail.com>
2024-12-16 20:48:14 +0545

    Managed issue in writing data to client

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const Gt06 = require('gt06')
 // TCP server configuration
 const tcpPort = 8090;
 
+// const byte = new Buffer.from('78781101086961106002852380411f4100007365f0d0a', 'hex')
+// const gt06 = new Gt06()
+// gt06.parse(byte)
+// console.log("++++++++++++++++++++++>", gt06)
+
 // Create a TCP server
 const server = net.createServer((socket) => {
   const gt06 = new Gt06();
@@ -21,11 +26,11 @@ const server = net.createServer((socket) => {
     }
 
     if (gt06.expectsResponse) {
-      console.log(gt06.responseMsg.toString('utf-8'));
+      socket.write(gt06.responseMsg)
     }
 
     gt06.msgBuffer.forEach(msg => {
-      console.log({ ...msg, responseMsg: Buffer.from(msg.responseMsg, 'utf-8') });
+      console.log(msg);
     });
 
     gt06.clearMsgBuffer();
