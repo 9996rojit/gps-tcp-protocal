c500d1db9b938340d0140abafe91a006a636baf3
999rojit <rockman9996@gmail.com>
2025-05-25 09:08:55 +0545

    Fixed code to connect new client

--- index.js
+++ index.js
@@ -1,41 +1,21 @@
 // Import necessary modules
 const net = require("net");
-const Gt06 = require('gt06');
 
 const tcpPort = 8090;
-const data = new Buffer.from('78781f1211071403362aca0543ec4f00ff976e021549010603e6b500e7590074763d0d0a', 'hex')
-const gt06 = new Gt06()
-gt06.parse(data)
-console.log("============>", gt06.msgBuffer)
 
 // Create a TCP server
 const server = net.createServer((socket) => {
-  const gt06 = new Gt06();
   console.log("New client connected:", socket.remoteAddress);
 
   // Event: Data received from client
   socket.on('data', (data) => {
     try {
-      console.log(data.toString('hex'))
-      gt06.parse(data);
+      console.log(data)
     }
     catch (e) {
       console.log('err', e);
       return;
     }
-
-    if (gt06.expectsResponse && gt06.event?.string === 'login') {
-      socket.write(gt06.responseMsg)
-    }
-    if (gt06.expectsResponse && gt06.event?.string === 'status') {
-      socket.write(gt06.responseMsg)
-    }
-
-    gt06.msgBuffer.forEach(msg => {
-      console.log(msg);
-    });
-
-    gt06.clearMsgBuffer();
   });
 
 
